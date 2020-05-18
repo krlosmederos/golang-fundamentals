668dcc63610037298933cac44582f8de394e4a9f
krlosmederos <krlosmederos@gmail.com>
2020-05-18 00:40:14 -0400

    Added closures function

---
+++ closures/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var val int
+	fmt.Scanf("%d", &val)
+	myFunc := Product(val)
+	for i := 0; i < 10; i++ {
+		fmt.Println(myFunc())
+	}
+}
+
+// Product is a function that return an other function
+func Product(num int) func() int {
+	counter := 0
+	return func() int {
+		counter++
+		return num * counter
+	}
+}
